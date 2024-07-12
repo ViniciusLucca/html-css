d3e7f39e8e528cd0a9686a96040423acfb87bbb7
ViniciusLucca <vinilucca7304@gmail.com>
2024-07-11 22:05:03 -0300

    Summary 1

---
+++ modulo-01/exercicios/ex006/index2 - Copia.js
@@ -0,0 +1,5 @@
+let paragraphs = document.getElementsByTagName('p')
+
+for (let index = 0; index < paragraphs.length; index++) {
+    paragraphs[index].textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias quasi in corporis voluptatibus quia libero nemo! Totam, quidem et! Enim error nulla sunt quibusdam officiis officia, assumenda ab exercitationem in.";
+}
